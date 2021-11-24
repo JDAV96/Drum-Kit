e52f5b9baf3e572f9345c9dcb2049a408d9b8a98
Jose Arevalo <arevalojose2015@outlook.com>
2021-11-24 15:58:52 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,66 @@
+document.querySelectorAll(".drum").forEach((element) =>
+                                         {
+                                           element.addEventListener("click", () =>
+                                           {
+                                             makeSound(element.innerText);
+                                             pressedAnimation(element.innerText);
+                                           });
+                                         });
+
+document.addEventListener("keydown", (e) =>
+{
+  if (makeSound(e.key))
+  {
+    pressedAnimation(e.key);
+  }
+});
+
+function makeSound(key)
+{
+  var soundMade = true;
+  switch (key)
+  {
+    case "w":
+    var audio = new Audio("sounds/crash.mp3");
+    audio.play();
+    break;
+    case "a":
+    var audio = new Audio("sounds/tom-1.mp3");
+    audio.play();
+    break;
+    case "s":
+    var audio = new Audio("sounds/tom-2.mp3");
+    audio.play();
+    break;
+    case "d":
+    var audio = new Audio("sounds/snare.mp3");
+    audio.play();
+    break;
+    case "j":
+    var audio = new Audio("sounds/tom-3.mp3");
+    audio.play();
+    break;
+    case "k":
+    var audio = new Audio("sounds/tom-4.mp3");
+    audio.play();
+    break;
+    case "l":
+    var audio = new Audio("sounds/kick-bass.mp3");
+    audio.play();
+    break;
+    default:
+    console.log(key);
+    soundMade = false;
+    break;
+  }
+
+  return soundMade;
+}
+
+function pressedAnimation(keyPressed)
+{
+  var activeButton = document.querySelector("." + keyPressed);
+  activeButton.classList.toggle("pressed");
+
+  setTimeout(() => {activeButton.classList.toggle("pressed");}, 100);
+}
